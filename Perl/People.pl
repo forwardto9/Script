5838e0520945997a140413426f75cf7766682f75
uweiyuan(袁有为) <uweiyuan@tencent.com>
2017-12-01 20:52:17 +0800

    add perl demo

---
+++ Perl/People.pl
@@ -0,0 +1,25 @@
+package MyPackage;
+sub new {
+    my $class = shift;
+    my $property = {
+      "name" => undef,
+      "age" => undef
+    };
+
+bless($property, $class);
+
+sub setPackage {
+  my $self = shift;
+  my ($name, $age) = @_;
+  $self->{"name"} = $name;
+  $self->{"age"} = $age;
+};
+
+sub getPackage {
+    $self = shift;
+   while (($key, $value) = each(%$self)) {
+    printf "result $key:$value\n";
+   }
+}
+}
+1;
