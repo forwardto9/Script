47493e6b29714ef53743ab8a0d1b8f2fbb32c314
uwei <forwardto9@gmail.com>
2017-12-04 23:01:10 +0800

    add mode

--- Perl/Perl-5.pl
+++ Perl/Perl-5.pl
@@ -17,6 +17,13 @@ foreach my $argv (@ARGV) {
     printf ("argv[%d] is %s\n", $argvIndex, $argv);
 }
 
+printf ("\@INC is @INC \n");
+
+foreach my $key (keys(%ENV)){
+printf "$key\n";
+}
+printf "Your login name is $ENV{'LOGNAME'}\n";
+
 #while(<>) {
 #    print "$.\t$_";
 #    if (eof){
@@ -125,3 +132,23 @@ __DATA__
 0x0000C518	0x0000003C	[ 11] -[MXGSetting enableDebug:]
 0x0000C554	0x0000002C	[ 11] -[MXGSetting isEnableDebug]
 0x0000C580	0x00000078	[ 11] -[MXGSetting changeToDebugServer]
+0x00006480	0x000001A8	[  3] -[MXGJceInputStream readFloat:]
+0x00006628	0x00000194	[  3] -[MXGJceInputStream readDouble:]
+0x000067BC	0x00000054	[  3] -[MXGJceInputStream readFloat]
+0x00006810	0x00000060	[  3] -[MXGJceInputStream readDouble]
+0x00006870	0x00000150	[  3] -[MXGJceInputStream readBytes:]
+0x000069C0	0x00000038	[  3] -[MXGJceInputStream skip:]
+0x000069F8	0x00000040	[  3] -[MXGJceInputStream readDataWithSize:]
+0x00006A38	0x0000032C	[  3] -[MXGJceInputStream readNumber:required:]
+0x00006D64	0x0000024C	[  3] -[MXGJceInputStream readString:required:]
+0x00006FB0	0x00000230	[  3] -[MXGJceInputStream readData:required:]
+0x000071E0	0x0000049C	[  3] -[MXGJceInputStream readObject:required:description:]
+0x0000767C	0x00000204	[  3] -[MXGJceInputStream readArray:required:description:]
+0x00007880	0x00000254	[  3] -[MXGJceInputStream readDictionary:required:description:]
+0x00007AD4	0x00000278	[  3] -[MXGJceInputStream readAnything:required:description:]
+0x00007D4C	0x00000010	[  3] -[MXGJceInputStream headType]
+0x00007D5C	0x00000010	[  3] -[MXGJceInputStream setHeadType:]
+0x00007D6C	0x00000010	[  3] -[MXGJceInputStream headTag]
+0x00007D7C	0x00000010	[  3] -[MXGJceInputStream dataHolders]
+0x00007D8C	0x0000001C	[  3] -[MXGJceInputStream setDataHolders:]
+0x00007DA8	0x000001A4	[  4] +[MXGJceObject jcePropertiesWithEncodedTypes]
