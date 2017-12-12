7f268f2ccfe0714a4e696e5a7e1baf4afb147ea8
uweiyuan(袁有为) <uweiyuan@tencent.com>
2017-12-12 16:40:11 +0800

    add array type in People.pl

--- Perl/People.pl
+++ Perl/People.pl
@@ -1,24 +1,37 @@
 package MyPackage;
 sub new {
-    my $class = shift;
-    my $property = {
-      "name" => undef,
-      "age" => undef
-    };
+	my $class = shift;
+	my $property = {
+	  "name" => undef,
+	  "age" => undef,
+	  "array" => undef
+	};
   return bless($property, $class);
 }
 
 sub setPackage {
   my $self = shift;
-  my ($name, $age) = @_;
+  my ($name, $age, $arr) = @_;
   $self->{"name"} = $name;
   $self->{"age"} = $age;
+  $self->{"array"} = $arr;
 };
 
 sub getPackage {
-    $self = shift;
+	$self = shift;
    while (($key, $value) = each(%$self)) {
-    printf "result $key:$value\n";
+		if(ref($value) eq "ARRAY") {
+			printf "result $key:";
+			@v = @{$value};
+			#$#v得到的是数组最后一个索引，不是数组的大小
+			$count = @v;
+			for ($i = 0; $i < $count; $i++) {
+				printf("value[%d] = %s ", $i, $$value[$i]);
+			}
+			printf ("\n");
+		} else {
+			printf "result $key:$value\n";
+		}
    }
 }
 1;
