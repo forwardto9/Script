2ab1674f53109969da65a753b2b11180e865aefa
uwei <forwardto9@gmail.com>
2018-05-02 14:45:31 +0800

    update

--- Perl/Perl-1.pl
+++ Perl/Perl-1.pl
@@ -1,33 +1,39 @@
 #!/usr/local/bin/perl
 print "hi perl\n";
 
-format EMPLOYEE =
+
+my $name, my $age, my $salary;
+
+format myformat =
 ===================================
-@<<<<<<<<<<<<<<<<<<<<<< @<< 
+@<<<<<<<<<<<<<<<<<<<<<< @<<
 $name, $age
 @#####.##
 $salary
 ===================================
 .
- 
+
 select(STDOUT);
-$~ = EMPLOYEE;
- 
-@n = ("Ali", "Runoob", "Jaffer");
-@a  = (20,30, 40);
-@s = (2000.00, 2500.00, 4000.000);
-%hashMap = (
+$~ = "myformat";
+
+my @n = ("Ali", "Runoob", "Jaffer");
+my @a  = (20,30, 40);
+my @s = (2000.00, 2500.00, 4000.000);
+my %hashMap = (
     "k1" => "V1",
     "k2" => "V2",
     "k3" => "V3",
     "k4" => "V4"
             );
- 
-$i = 0;
-while (($key, $value) =  each(%hashMap)) {
+
+my $i = 0;
+while (my ($key, $value) =  each(%hashMap)) {
     $name = $key;
     $age = $value;
     write;
     #printf ("%s:%s\n", $key, $value);
 }
 
+my $testString = "hi Atom";
+printf($testString);
+printf($testString);
